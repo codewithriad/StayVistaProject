5b4f7d15e0324cf692bf879b6f2538178ac2a0a8
solugionmaker99 <frriyad111@gmail.com>
2024-03-28 13:57:03 +0600

    added stavista for first time

---
+++ client/src/components/Rooms/Rooms.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Card from "../Card/Card";
+import Container from "../Shared/Container";
+
+const Rooms = () => {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    fetch("./rooms.json")
+      .then((res) => res.json())
+      .then((data) => setRooms(data));
+  }, []);
+
+  return (
+    <Container>
+      <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {rooms.map((room) => (
+            <Card key={room._id} id={room._id} room={room} />
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Rooms;
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Rooms from "../../components/Rooms/Rooms";
+
+const Home = () => {
+  return (
+    <div>
+      {/* Categories section */}
+      {/* Rooms Section */}
+      <Rooms />
+    </div>
+  );
+};
+
+export default Home;
