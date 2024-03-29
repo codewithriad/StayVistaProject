072fee42b293be9e4022db2b4221c6b67c181b65
solugionmaker99 <frriyad111@gmail.com>
2024-03-29 12:12:48 +0600

    query-string added

--- client/src/components/Rooms/Categories/CategoryBox.jsx
+++ client/src/components/Rooms/Categories/CategoryBox.jsx
@@ -1,7 +1,27 @@
-// eslint-disable-next-line react/prop-types, no-unused-vars
+import { useNavigate, useSearchParams } from "react-router-dom";
+import qs from "query-string";
+
+// eslint-disable-next-line react/prop-types
 const CategoryBox = ({ label, icon: Icon }) => {
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+  const handleParams = () => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+      const updatedQuery = { ...currentQuery, category: label };
+      const url = qs.stringifyUrl({
+        url: "/",
+        query: updatedQuery,
+      });
+      navigate(url);
+    }
+  };
   return (
-    <div className="flex flex-col justify-center items-center gap-2 p-3 border-b-2 hover:text-neutral-600 transition cursor-pointer">
+    <div
+      onClick={handleParams}
+      className="flex flex-col justify-center items-center gap-2 p-3 border-b-2 hover:text-neutral-600 transition cursor-pointer"
+    >
       <Icon size={26} />
       <div className="text-sm font-medium">{label}</div>
     </div>
