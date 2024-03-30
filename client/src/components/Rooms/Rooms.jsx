3336707ded3fda15c4865949a8ce87281d2d5f4d
solugionmaker99 <frriyad111@gmail.com>
2024-03-30 15:29:56 +0600

    added headding component on rooms component if category inclues nothing

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -23,29 +23,24 @@ const Rooms = () => {
   }, [category]);
 
   return (
-    // <Container>
-    //   {rooms && rooms.length > 0 ? (
-    //     <div>
-    //       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-    //         {rooms.map((room) => (
-    //           <Card key={room._id} id={room._id} room={room} />
-    //         ))}
-    //       </div>
-    //     </div>
-    //   ) : (
-    //     <Heading
-    //       center={true}
-    //       title={"No Rooms Available In This Category!"}
-    //       subtitle={"Plese Select Other Categories."}
-    //     />
-    //   )}
-    // </Container>
-    <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {rooms.map((room) => (
-          <Card key={room._id} id={room._id} room={room} />
-        ))}
-      </div>
+    <div className="flex justify-center items-center min-h-[calc(100vh-300px)]">
+      <Container>
+        {rooms && rooms.length > 0 ? (
+          <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+              {rooms.map((room) => (
+                <Card key={room._id} id={room._id} room={room} />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <Heading
+            center={true}
+            title={"No Rooms Available In This Category!"}
+            subtitle={"Please Select Another Category."}
+          />
+        )}
+      </Container>
     </div>
   );
 };
