dd40cfcbe3387c4e6d0ec0ad01a8e28bb9ea2d05
solugionmaker99 <frriyad111@gmail.com>
2024-04-21 10:59:14 +0600

    added imagebb for upload image on website easily

--- client/src/components/RoomDetails/RoomReservation.jsx
+++ client/src/components/RoomDetails/RoomReservation.jsx
@@ -4,18 +4,29 @@
 import { useState } from "react";
 import Button from "../Button/Button";
 import Calender from "./Calender";
+import { formatDistance } from "date-fns";
 
 // eslint-disable-next-line no-unused-vars
 const RoomReservation = ({ room }) => {
-  const [value, setValue] = useState({
-    startDate: new Date(room?.from),
-    endDate: new Date(room?.to),
-    key: "selection",
-  });
+  // const [value, setValue] = useState({
+  //   startDate: new Date(room?.from),
+  //   endDate: new Date(room?.to),
+  //   key: "selection",
+  // });
 
-  const handleSelect = (ranges) => {
-    setValue({ ...value });
-  };
+  // const handleSelect = (ranges) => {
+  //   setValue({ ...value });
+  // };
+
+  // Total Days Calculation
+
+  // const totalDays = parseInt(
+  //   formatDistance(new Date(room?.to), new Date(room?.from)).split(" ")[0]
+  // );
+
+  // Total Price
+
+  // const totalPrice = totalDays * room?.price;
 
   return (
     <div className="rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white">
@@ -27,14 +38,6 @@ const RoomReservation = ({ room }) => {
         <Calender />
       </div>
 
-      {/* <div className="flex items-center gap-1 p-4">
-        <div className="text-2xl font-semibold">$ {room?.price}</div>
-        <div className="font-light text-neutral-600">night</div>
-      </div>
-      <hr /> */}
-      {/* <div className="flex justify-center">
-        <Calender handleSelect={handleSelect} value={value} />
-      </div> */}
       <hr />
       <div className="p-4">
         <Button label={"Reserve"} />
@@ -43,7 +46,7 @@ const RoomReservation = ({ room }) => {
 
       <div className="p-4 flex items-center justify-between font-semibold text-2xl">
         <div>Total</div>
-        <div>$ {room?.price}</div>
+        {/* <div>$ {totalPrice}</div> */}
       </div>
     </div>
   );
